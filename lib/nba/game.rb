f3a55383fa5cc172997728ab3419e21513c585d4
Larry Lv <larrylv1990@gmail.com>
2014-01-05 11:24:46 +0800

    Refactor game.

--- lib/nba/game.rb
+++ lib/nba/game.rb
@@ -20,15 +20,16 @@ module NBA
       print(games)
     end
 
-    def pretty_format(options = {})
-      self.status.center(options[:status_length].to_i + 1) + " - " + \
-        self.home_team.center(options[:home_length].to_i + 1) + " " + \
-        (self.home_score =~ /\d/ ? self.home_score.center(4) : "") + " : " + \
-        (self.away_score =~ /\d/ ? self.away_score.center(4) : "") + " " + \
-        self.away_team.center(options[:away_length].to_i + 1)
+    def pretty_print(options = {})
+      puts self.status.center(options[:status_length].to_i + 1) + " - " + \
+             self.home_team.center(options[:home_length].to_i + 1) + " " + \
+             self.home_score.center(options[:home_score].to_i + 1) + " : " + \
+             self.away_score.center(options[:away_score].to_i + 1) + " " + \
+             self.away_team.center(options[:away_length].to_i + 1)
     end
 
     private
+
       def initialize(attributes = {})
         attributes.each do |key, value|
           instance_variable_set("@#{key}", value) if self.respond_to?(key)
@@ -39,13 +40,15 @@ module NBA
         {
           :status_length => games.map(&:status).map(&:length).max,
           :home_length   => games.map(&:home_team).map(&:length).max,
+          :home_score    => games.map(&:home_score).map(&:length).max,
           :away_length   => games.map(&:away_team).map(&:length).max,
+          :away_score    => games.map(&:away_score).map(&:length).max
         }
       end
 
       def self.print(games)
         length_options = length_options_of(games)
-        puts games.map { |game| game.pretty_format(length_options) }.join("\n")
+        games.each { |game| game.pretty_print(length_options) }
       end
 
       def self.results_to_game(results)
